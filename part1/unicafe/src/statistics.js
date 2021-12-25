bdf813b23087564bae12d6b3b1e40e81a7af9bca
Daniel Calderón <daniel.calderon.portillo@gmail.com>
2021-12-25 16:44:21 +0100

    1.9 Unicafe

--- part1/unicafe/src/statistics.js
+++ part1/unicafe/src/statistics.js
@@ -1,15 +1,23 @@
 const Statistics = (props) => {
   const {good, neutral, bad} = props;
 
+  const all = good + neutral + bad;
+
+  if (good || neutral || bad) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good {good} </p>
+        <p>neutral {neutral} </p>
+        <p>bad {bad} </p>
+        <p>average {(good - bad) / all}</p>
+        <p>positive {(good * 100) / all}%</p>
+      </>
+    )
+  }
+
   return (
-    <>
-      <h2>statistics</h2>
-      <p>good {good} </p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad} </p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {(good * 100) / (good + bad + neutral)}%</p>
-    </>
+    <p>No feedback given</p>
   )
 };
 
