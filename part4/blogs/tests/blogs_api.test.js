3e53b8e503c1c41b8c20ca8e810f8632fa226f94
Daniel Calderón <daniel.calderon.portillo@gmail.com>
2021-12-29 18:22:08 +0100

    4.8

---
+++ part4/blogs/tests/blogs_api.test.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let index = 0; index < initialBlogs.length; index++) {
+    let blogObject = new Blog(initialBlogs[index])
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog is within the returned notes', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(blog => blog.title)
+  expect(titles).toContain(
+    'React patterns'
+  )
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- part4/blogs/utils/logger.js
+++ part4/blogs/utils/logger.js
@@ -1,5 +1,8 @@
 const info = (...params) => {
-  console.log(...params)
+  // eslint-disable-next-line no-undef
+  if (process.env.NODE_ENV !== 'test') { 
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
