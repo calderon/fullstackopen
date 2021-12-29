f15588108a477732b5bcb9f6f9335d27b3dc034e
Daniel Calderón <daniel.calderon.portillo@gmail.com>
2021-12-29 16:25:22 +0100

    4.4

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  const sumLikes = (sum, currentBlog) => sum + currentBlog.likes
+
+  return blogs.reduce(sumLikes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
